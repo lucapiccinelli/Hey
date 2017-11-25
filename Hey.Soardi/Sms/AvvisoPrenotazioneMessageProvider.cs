2c16bead486a10d452dd401d57909842467ea107
lucapiccinelli <luca.picci@gmail.com>
2017-11-25 16:48:21 +0100

    aggiunto sms sender per prenotazione riparazione

---
+++ Hey.Soardi/Sms/AvvisoPrenotazioneMessageProvider.cs
@@ -0,0 +1,17 @@
+﻿using Hey.Core;
+
+namespace Hey.Soardi.Sms
+{
+    public class AvvisoPrenotazioneMessageProvider : IMessageProvider
+    {
+        public string GetText()
+        {
+            return "Info Soardi, Le ricordiamo l'appuntamento di domani mattina per la riparazione della Sua Vettura.";
+        }
+
+        public string GetAbstract()
+        {
+            return "Promemoria riparazione auto.";
+        }
+    }
+}
--- Hey.Soardi/Sms/SoardiSmsSender.cs
+++ Hey.Soardi/Sms/SoardiSmsSender.cs
@@ -3,26 +3,27 @@ using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Hey.Core;
 using Hey.Core.Attributes;
 using Hey.Core.Services;
 using Hey.Soardi.Sms.Exceptions;
 
 namespace Hey.Soardi.Sms
 {
-    public class NoteVeicoloSmsSender
+    public class SoardiSmsSender
     {
         private readonly INotEnoughCredit _notEnoughCredit;
         private readonly string _username;
         private readonly string _password;
         private readonly string _from;
 
-        public NoteVeicoloSmsSender()
+        public SoardiSmsSender()
             :this(new NotEnoughCreditEmail(ConfigurationManager.AppSettings["SmsCreditWatcherEmail"]))
         {
             
         }
 
-        public NoteVeicoloSmsSender(INotEnoughCredit notEnoughCredit)
+        public SoardiSmsSender(INotEnoughCredit notEnoughCredit)
         {
             _notEnoughCredit = notEnoughCredit;
             _username = ConfigurationManager.AppSettings["SmsUser"];
@@ -38,9 +39,25 @@ namespace Hey.Soardi.Sms
 
         public void Send(long idNota, string receiverPhoneNumber, string connection, ISenderService senderService)
         {
+            SendSms(
+                receiverPhoneNumber, 
+                senderService, 
+                new NoteVeicoloMessageProvider((int)idNota, Connections.Strings[connection]));
+        }
+
+        [FireMe("Prenotazione")]
+        public void Send(string receiverPhoneNumber)
+        {
+            SendSms(
+                receiverPhoneNumber, 
+                new SmsServiceSender(_username, _password, _from),
+                new AvvisoPrenotazioneMessageProvider());
+        }
+
+        private void SendSms(string receiverPhoneNumber, ISenderService senderService, IMessageProvider messageProvider)
+        {
             try
             {
-                var messageProvider = new NoteVeicoloMessageProvider((int)idNota, Connections.Strings[connection]);
                 var noteVeicoloSender = new NoteVeicoloSender(senderService, messageProvider);
                 noteVeicoloSender.Send(receiverPhoneNumber);
             }
